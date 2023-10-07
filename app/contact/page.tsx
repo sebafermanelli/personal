d38aca7747c24b65bbbed58b115989162ae3da04
Sebastian Fermanelli <sebastianfermanelli@gmail.com>
2023-10-07 15:06:32 -0300

    Contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,13 +1,47 @@
-import Image from 'next/image';
+'use client';
+
+import Button from '@/components/Button';
+import ErrorMessage from '@/components/ErrorMessage';
+import SuccessMessage from '@/components/SuccessMessage';
 
 export default function Home() {
+	const handleSubmitContact = () => {
+		// logica para enviar el email y conectar con la api
+	};
+
 	return (
-		<main className="h-full w-full p-2">
-			<section className="flex flex-col items-stretch justify-stretch">
-				<div className="flex items-center justify-center">
-					<Image src="/assets/icons/wrench.svg" alt="wrench" width={30} height={30} className="object-contain mx-2" />
-					<span className="text-2xl sm:text-4xl text-gradient-primary">Under construction</span>
-				</div>
+		<main className="flex-1 h-full w-full p-2">
+			<section className="flex flex-col items-center justify-center">
+				<span className="text-2xl sm:text-4xl text-gradient-primary">Contact me</span>
+
+				<form className="flex flex-col w-full md:w-1/2" aria-required>
+					<label htmlFor="name" className="text-l mt-3 ms-2">
+						Name
+					</label>
+					<input type="text" name="name" id="name" placeholder="" className="input" required />
+
+					<label htmlFor="email" className="text-l mt-3 ms-2">
+						Email
+					</label>
+					<input type="email" name="email" id="email" placeholder="" className="input" required />
+
+					<label htmlFor="subject" className="text-l mt-3 ms-2">
+						Subject
+					</label>
+					<input type="text" name="subject" id="subject" placeholder="" className="input" required />
+
+					<label htmlFor="message" className="text-l mt-3 ms-2">
+						Message
+					</label>
+					<textarea name="message" id="message" placeholder="" className="input" required />
+
+					<div className="flex justify-end mt-3">
+						<Button text="Send" onClick={handleSubmitContact} />
+					</div>
+				</form>
+
+				{/* <SuccessMessage message="I received your email" />
+				<ErrorMessage message="An error occurred" /> */}
 			</section>
 		</main>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
+'use client';
+
+import Button from '@/components/Button';
 import Image from 'next/image';
 
 export default function Home() {
+	const handleDownloadResume = () => {
+		const downloadLink = document.createElement('a');
+		downloadLink.href = '/assets/resume.pdf';
+		downloadLink.download = 'sebastian_fermanelli_resume.pdf';
+		downloadLink.click();
+	};
+
+	const handleContactButton = () => {
+		const contactLink = document.createElement('a');
+		contactLink.href = 'mailto:sebastianfermanelli@gmail.com';
+		contactLink.click();
+	};
+
 	return (
 		<main className="flex-1 h-full w-full p-2">
 			<section className="flex flex-col items-stretch justify-stretch text-justify">
@@ -47,110 +63,115 @@ export default function Home() {
 				</div>
 
 				<div className="my-5 flex gap-14 items-center justify-center">
-					<a
-						href="/sebastian_fermanelli_resume.pdf"
-						download="Sebastian Fermanelli Resume.pdf"
-						className="bg-black text-white py-2 px-4 rounded-3xl border border-black hover:bg-transparent hover:text-black transition-all duration-500"
-					>
-						Resume
-					</a>
+					<Button text="Resume" onClick={handleDownloadResume} />
 
-					<a
-						href="mailto:sebastianfermanelli@gmail.com"
-						className="bg-black text-white py-2 px-4 rounded-3xl border border-black hover:bg-transparent hover:text-black transition-all duration-500"
-					>
-						Contact
-					</a>
+					<Button text="Contact" onClick={handleContactButton} />
 				</div>
 
 				<div className="my-2 flex flex-col gap-4 items-center justify-center">
 					<span className="text-xl text-gradient-primary">Technologies</span>
-					<div className="flex gap-10">
-						<Image
-							src="/assets/icons/technologies/js.svg"
-							alt="js logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
+					<div className="flex flex-wrap items-center justify-center">
+						{/* Languages */}
+						<div className="flex items-center justify-center m-4">
+							<Image
+								src="/assets/icons/technologies/js.svg"
+								alt="js logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
 
-						<Image
-							src="/assets/icons/technologies/ts.svg"
-							alt="ts logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-
-						<Image
-							src="/assets/icons/technologies/java.svg"
-							alt="java logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-					</div>
-
-					<div className="flex gap-10">
-						<Image
-							src="/assets/icons/technologies/reactjs.svg"
-							alt="reactjs logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-					</div>
-
-					<div className="flex gap-10">
-						<Image
-							src="/assets/icons/technologies/nodejs.svg"
-							alt="nodejs logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-
-						<Image
-							src="/assets/icons/technologies/expressjs.svg"
-							alt="expressjs logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-
-						<Image
-							src="/assets/icons/technologies/nextjs.svg"
-							alt="nextjs logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-
-						<Image
-							src="/assets/icons/technologies/spring.svg"
-							alt="spring logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-					</div>
-
-					<div className="flex gap-10">
-						<Image
-							src="/assets/icons/technologies/mysql.svg"
-							alt="mysql logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
-
-						<Image
-							src="/assets/icons/technologies/mongodb.svg"
-							alt="mongodb logo"
-							width={50}
-							height={50}
-							className="object-contain hover:mx-3 transition-all duration-500"
-						/>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/ts.svg"
+								alt="ts logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/java.svg"
+								alt="java logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						{/* Frontend technologies */}
+						<div className="flex items-center justify-center m-4">
+							<Image
+								src="/assets/icons/technologies/reactjs.svg"
+								alt="reactjs logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						{/* Backend technologies */}
+						<div className="flex items-center justify-center m-4">
+							<Image
+								src="/assets/icons/technologies/nodejs.svg"
+								alt="nodejs logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/expressjs.svg"
+								alt="expressjs logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/nextjs.svg"
+								alt="nextjs logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/spring.svg"
+								alt="spring logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						{/* Data querying technologies */}
+						<div className="flex items-center justify-center m-4">
+							<Image
+								src="/assets/icons/technologies/mysql.svg"
+								alt="mysql logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
+						<div className="flex items-center justify-center m-4">
+							{' '}
+							<Image
+								src="/assets/icons/technologies/mongodb.svg"
+								alt="mongodb logo"
+								width={50}
+								height={50}
+								className="object-contain"
+							/>
+						</div>
 					</div>
 				</div>
 			</section>
