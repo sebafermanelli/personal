17b5d8c2f54334afe1e34754acf19c51137d62f5
sebafermanelli <sebastianfermanelli@gmail.com>
2024-06-12 13:26:36 -0300

    Personal page refactoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,29 @@
-import '@/styles/globals.css';
-import Nav from '@/components/Nav';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Footer from '@/components/Footer';
+import "@/styles/global.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { Toaster } from "@/components/ui/toaster";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'Sebastian Fermanelli',
-	description: 'Sebastian Fermanelli Portfolio',
+    title: "Sebastian Fermanelli",
+    description: "Sebastian Fermanelli Portfolio",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<div className="main">
-					<div className="gradient"></div>
-				</div>
-				<main className="app">
-					<Nav />
-					{children}
-					<Footer />
-				</main>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    <Header />
+                    <main className="container mx-auto px-3 max-w-screen-lg">{children}</main>
+                    <Footer />
+                    <Toaster />
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,22 @@
-'use client';
+"use client";
 
-import AboutMe from '@/components/AboutMe';
-import TechnologiesImg from '@/components/TechnologiesImg';
-import Image from 'next/image';
+import { AboutMe } from "@/components/about-me";
+import { ContactForm } from "@/components/contact-form";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
-	const handleDownloadResume = () => {
-		const downloadLink = document.createElement('a');
-		downloadLink.href = '/assets/resume.pdf';
-		downloadLink.download = 'sebastian_fermanelli_resume.pdf';
-		downloadLink.click();
-	};
+    return (
+        <>
+            <div className="flex justify-center items-center mb-6">
+                <div className="text-center inline-block space-y-2">
+                    <h1 className="text-4xl font-semibold">Sebastian Fermanelli</h1>
+                    <Separator className="w-full" />
+                    <p>Systems Engineering student and Back End Engineer from Argentina</p>
+                </div>
+            </div>
 
-	const handleContactButton = () => {
-		const contactLink = document.createElement('a');
-		contactLink.href = '/contact';
-		contactLink.click();
-	};
-
-	return (
-		<main className="flex-1 h-full w-full p-2">
-			<section className="flex flex-col text-justify">
-				<div className="text-xl sm:text-2xl my-2">
-					<span>Hi there! I'm</span>
-					<h1>
-						<span className="text-2xl sm:text-4xl text-gradient-black">Sebastian</span>,
-					</h1>
-					<span>
-						a Systems Engineering student and a Full Stack developer from
-						<div className="flex">
-							Argentina
-							<Image
-								src="/assets/images/argentina.svg"
-								alt="argentina flag"
-								width={30}
-								height={30}
-								className="object-contain mx-2"
-							/>
-						</div>
-					</span>
-				</div>
-
-				<AboutMe />
-
-				<div className="flex-center gap-14 my-5">
-					<button className="btn-black" onClick={handleDownloadResume}>
-						Resume
-					</button>
-
-					<button className="btn-black" onClick={handleContactButton}>
-						Contact
-					</button>
-				</div>
-
-				<div className="flex-center flex-col gap-2">
-					<span className="text-xl text-gradient-black">Technologies</span>
-					<TechnologiesImg />
-				</div>
-			</section>
-		</main>
-	);
+            <AboutMe />
+            <ContactForm />
+        </>
+    );
 }
