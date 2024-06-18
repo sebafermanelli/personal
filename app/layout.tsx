9f2d46fc10c0fe999987528c719988258eed8a82
sebafermanelli <sebastianfermanelli@gmail.com>
2024-06-18 20:22:38 -0300

    deleted animated bg due performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                             <Footer />
                             <Toaster />
                         </div>
-                        <BackgroundBeams />
+                        {/* <BackgroundBeams /> */}
                     </div>
                 </ThemeProvider>
             </body>
